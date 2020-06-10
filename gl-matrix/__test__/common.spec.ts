78d11f35d021d89031d0981313f04a1fe25b5c91
Jiahao Li <lijiahao_shandong@163.com>
2020-06-10 11:09:37 +0800

    [Test] Add test case of common.ts

--- gl-matrix/__test__/common.spec.ts
+++ gl-matrix/__test__/common.spec.ts
@@ -1,4 +1,8 @@
-import { equal, toRadian } from '../src/common';
+import { equal, toRadian, ARRAY } from '../src/common';
+
+it('should get Float32Array', () => {
+  expect(ARRAY).toEqual(Float32Array);
+});
 
 it('should get 2π when input 360 degrees', () => {
   expect(toRadian(360)).toEqual(2 * Math.PI);
@@ -6,6 +10,10 @@ it('should get 2π when input 360 degrees', () => {
 
 it('should get true when compare 1997 to 1997.0000001', () => {
   expect(equal(1997, 1997.0000001)).toBeTruthy();
+});
+
+it('should get true when compare 1997 to 1997.00000001', () => {
+  expect(equal(1997, 1997.00000001)).toBeTruthy();
 });
 
 it('should get false when compare 1997 to 1997.0001', () => {
